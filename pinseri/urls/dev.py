cf069a4b335d2dc08ca1247f3b75307c0b8eb9ef
vincenzorizza <vincenzorizza6@gmail.com>
2014-05-16 12:35:13 +0100

    push requirements

--- pinseri/urls/dev.py
+++ pinseri/urls/dev.py
@@ -1,7 +1,7 @@
 from django.conf.urls.static import static
 from django.conf import settings
 
-from pinseri.urls import urlpatterns
+from pinseri.urls.base import urlpatterns
 
 urlpatterns += static(settings.STATIC_URL,
     document_root=settings.STATIC_ROOT)
