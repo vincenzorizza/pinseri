d05ab47eeb569ed4c0f9ab2c027c7e0c2b1cd39d
vincenzorizza <vincenzorizza6@gmail.com>
2014-05-18 23:06:28 +0100

    wsgi

--- pinseri/wsgi.py
+++ pinseri/wsgi.py
@@ -24,7 +24,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE",
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-print "WSGI"
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
