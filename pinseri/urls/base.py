dac3e2749d9bb406a6fe425f95346eca1c5ec356
vincenzorizza <vincenzorizza6@gmail.com>
2014-09-09 11:29:10 +0200

    14-09-09

--- pinseri/urls/base.py
+++ pinseri/urls/base.py
@@ -6,8 +6,13 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Home Page -- Replace as you prefer
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-
+    url(r'^$', TemplateView.as_view(template_name='comingsoon.html'), name='home'),
+    url(r'^prova/$', TemplateView.as_view(template_name='home.html'), name='prova'),
+    url(r'^prova/product/$', TemplateView.as_view(template_name='product.html'), name='product'),
+    url(r'^prova/service/$', TemplateView.as_view(template_name='service.html'), name='service'),
+    url(r'^prova/order/$', TemplateView.as_view(template_name='order.html'), name='order'),
+    url(r'^prova/contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    url(r'^prova/about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^thirdauth/$', TemplateView.as_view(template_name='thirdauth/home.html'), name='home'),
